212eb3e52d066c10313cde9b53d5d62a465e0a64
Alessandro-pang <pcy1314@163.com>
2023-07-06 20:58:38 +0800

    init: 初始化仓库

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Author: zi.yang
+ * @Date: 2023-07-04 11:24:27
+ * @LastEditors: zi.yang
+ * @LastEditTime: 2023-07-05 21:10:54
+ * @Description: 
+ * @FilePath: /vue-project/src/App.vue
+-->
+<script setup>
+import { nextTick } from 'vue'
+import { createGraph } from './graph';
+nextTick(()=> createGraph("main"))
+</script>
+
+<template>
+  <main id="main"></main>
+</template>
+
+<style scoped>
+#main {
+  height: 100%;
+}
+</style>
