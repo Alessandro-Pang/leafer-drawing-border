cf39bc5c572e7684b641ccd3134cf6fa6bcdc017
Alessandro-pang <pcy1314@163.com>
2023-08-06 15:22:36 +0800

    chron: 修复发布到 github 资源找不到的问题

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
+/*
+ * @Author: zi.yang
+ * @Date: 2023-07-04 11:24:27
+ * @LastEditors: zi.yang
+ * @LastEditTime: 2023-08-06 15:21:45
+ * @Description: 
+ * @FilePath: /vue-project/vite.config.js
+ */
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig((command) => {
+  const base = command.mode === 'github' ? '/leafer-drawing-border/' : '/'
+  return {
+    base,
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
